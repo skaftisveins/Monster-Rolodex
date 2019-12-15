1b7ff316c4a81529198ca09f88655d5f08374d0b
skaftisveins <skaftisveins@gmail.com>
2019-12-15 22:25:10 +0000

    Updates

---
+++ precache-manifest.04af39e4d6c8b068bd5e26693b7ed176.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca150f9f20863e4f26688ba44f592e62",
+    "url": "/Monster-Rolodex/index.html"
+  },
+  {
+    "revision": "5a8add254f57af5ec446",
+    "url": "/Monster-Rolodex/static/css/main.b926d4d4.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/Monster-Rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Monster-Rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5a8add254f57af5ec446",
+    "url": "/Monster-Rolodex/static/js/main.66697720.chunk.js"
+  },
+  {
+    "revision": "1d1b38e19d7b14dc578d",
+    "url": "/Monster-Rolodex/static/js/runtime-main.02a9a234.js"
+  }
+]);
